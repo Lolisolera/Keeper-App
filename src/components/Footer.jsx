e3eab177719457fd9ff272271f1f274a27189823
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2025-04-08 12:32:22 +0100

    update footer.jsx file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+
+
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
+      <p>Created By Lola Marquez 👽</p>
       <p>Copyright ⓒ {year}</p>
+  
     </footer>
   );
 }
